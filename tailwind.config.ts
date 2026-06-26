e8516727e53a03ee848ae0e5f0f0e44e1f7e9d29
Doy679 <246489103+Doy679@users.noreply.github.com>
2026-06-26 20:41:33 +0800

    config: restrict active daisyui themes to light and dark

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,7 +27,7 @@ const config: Config = {
   },
   plugins: [
     daisyui({
-      themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+      themes: ["light", "dark"],
       darkTheme: "dark",
     }),
   ],
